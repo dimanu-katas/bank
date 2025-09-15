97bd81cbfea66ebf57089c39332e22d9d22cf678
dimanu-py <dimanu.py@gmail.com>
2025-09-15 18:09:13 +0200

    feat(bank-kata): implement 'record' method to store transactions in repository

--- bank-kata/solution/src/main/java/org/dimanu/bankkata/TransactionRepository.java
+++ bank-kata/solution/src/main/java/org/dimanu/bankkata/TransactionRepository.java
@@ -1,11 +1,14 @@
 package org.dimanu.bankkata;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionRepository {
 
+    private List<Transaction> transactions = new ArrayList<>();
+
     public void record(Transaction transaction) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        transactions.add(transaction);
     }
 
     public List<Transaction> getAllTransactions() {
