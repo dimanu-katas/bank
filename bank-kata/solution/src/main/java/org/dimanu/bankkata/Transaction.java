708d6b0e255bde3e8a6b8fbfd7a1e0d57cc0e4b3
dimanu-py <dimanu.py@gmail.com>
2025-09-15 15:42:53 +0200

    feat(bank-kata): add additional constructor to Transaction to be able to pass an optional field representing the date where the transaction was made

--- bank-kata/solution/src/main/java/org/dimanu/bankkata/Transaction.java
+++ bank-kata/solution/src/main/java/org/dimanu/bankkata/Transaction.java
@@ -4,20 +4,26 @@ import java.util.Objects;
 
 public class Transaction {
     private final int amount;
+    private final String date;
 
     public Transaction(int amount) {
+        this(amount, null);
+    }
+
+    public Transaction(int amount, String date) {
         this.amount = amount;
+        this.date = date;
     }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Transaction that = (Transaction) o;
-        return amount == that.amount;
+        return amount == that.amount && Objects.equals(date, that.date);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(amount);
+        return Objects.hash(amount, date);
     }
 }
