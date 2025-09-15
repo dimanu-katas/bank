4f3464367a78a6733aaa332ce793fe09ad0a7bef
dimanu-py <dimanu.py@gmail.com>
2025-09-15 18:55:05 +0200

    test(bank-kata): add test for printing statement with a single transaction

--- bank-kata/solution/src/test/java/org/dimanu/bankkata/StatementPrinterShould.java
+++ bank-kata/solution/src/test/java/org/dimanu/bankkata/StatementPrinterShould.java
@@ -4,12 +4,15 @@ package org.dimanu.bankkata;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collections;
 import java.util.List;
 
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,5 +33,18 @@ class StatementPrinterShould {
         statementPrinter.print(NO_TRANSACTIONS);
 
         verify(console).printLine(HEADER);
+    }
+
+    @Test
+    void print_statemen_with_only_one_transaction() {
+        List<Transaction> transactions = List.of(
+                TransactionMother.deposit(1000, "10/01/2025")
+        );
+
+        statementPrinter.print(transactions);
+
+        InOrder inOrder = inOrder(console);
+        inOrder.verify(console).printLine(HEADER);
+        inOrder.verify(console).printLine("10/01/2025 | 1000.00 | 1000.00");
     }
 }
