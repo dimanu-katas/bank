3016f3b4f4c1a3be581408fe93b29013e788d50a
dimanu-py <dimanu.py@gmail.com>
2025-09-15 18:45:58 +0200

    test(bank-kata): implement red test to verify statement printer prints only header to consoler when there are no transactions

---
+++ bank-kata/solution/src/test/java/org/dimanu/bankkata/StatementPrinterShould.java
@@ -0,0 +1,30 @@
+package org.dimanu.bankkata;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.verify;
+
+class StatementPrinterShould {
+    private final List<Transaction> NO_TRANSACTIONS = Collections.emptyList();
+
+    @Mock Console console;
+    private StatementPrinter statementPrinter;
+
+    @BeforeEach
+    void setUp() {
+        statementPrinter = new StatementPrinter();
+    }
+
+    @Test
+    void only_print_header_when_no_transactions() {
+        statementPrinter.print(NO_TRANSACTIONS);
+
+        verify(console).printLine("DATE | AMOUNT | BALANCE");
+    }
+}
