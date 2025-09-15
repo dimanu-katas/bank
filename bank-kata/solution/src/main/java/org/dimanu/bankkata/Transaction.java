2cdfedd69839a9f1897f3cf55454816a0610cb0e
dimanu-py <dimanu.py@gmail.com>
2025-09-15 12:51:01 +0200

    feat(bank-kata): implement equals and hashCode to be able to compare transactions

--- bank-kata/solution/src/main/java/org/dimanu/bankkata/Transaction.java
+++ bank-kata/solution/src/main/java/org/dimanu/bankkata/Transaction.java
@@ -1,9 +1,23 @@
 package org.dimanu.bankkata;
 
+import java.util.Objects;
+
 public class Transaction {
     private final int amount;
 
     public Transaction(int amount) {
         this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return amount == that.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(amount);
     }
 }
