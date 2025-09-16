d0e7513404945fe43f4b72247ea77209f7abf45a
dimanu-py <dimanu.py@gmail.com>
2025-09-16 09:04:39 +0200

    test(bank-kata): add test for printing statement with multiple transactions

--- bank-kata/solution/src/test/java/org/dimanu/bankkata/StatementPrinterShould.java
+++ bank-kata/solution/src/test/java/org/dimanu/bankkata/StatementPrinterShould.java
@@ -47,4 +47,21 @@ class StatementPrinterShould {
         inOrder.verify(console).printLine(HEADER);
         inOrder.verify(console).printLine("10/01/2025 | 1000.00 | 1000.00");
     }
+
+    @Test
+    void print_statement_with_multiple_transactions_in_chronological_reversed_order() {
+        List<Transaction> transactions = List.of(
+                TransactionMother.deposit(1000, "10/01/2025"),
+                TransactionMother.deposit(2000, "13/01/2025"),
+                TransactionMother.withdrawal(500, "14/01/2025")
+        );
+
+        statementPrinter.print(transactions);
+
+        InOrder inOrder = Mockito.inOrder(console);
+        inOrder.verify(console).printLine(HEADER);
+        inOrder.verify(console).printLine("14/01/2025 | -500.00 | 2500.00");
+        inOrder.verify(console).printLine("13/01/2025 | 2000.00 | 3000.00");
+        inOrder.verify(console).printLine("10/01/2025 | 1000.00 | 1000.00");
+    }
 }
